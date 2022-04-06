4a1813ce2cdf73b1713f58b84a0a4118bdcaf288
AlphaBryan <90648570+AlphaBryan@users.noreply.github.com>
2022-04-06 07:17:40 -0400

    test responsive betta interface

--- snapcycle/forms.py
+++ snapcycle/forms.py
@@ -3,4 +3,4 @@ from django import forms
 
 class EmailForm(forms.Form):
     email_address = forms.EmailField(label='', widget=forms.EmailInput(
-        attrs={'placeholder': 'Adresse Courriel', 'style': 'width: 90vh;height:10vh', 'class': 'form-control form-control-lg'}))
+        attrs={'placeholder': 'Adresse Courriel', 'style': 'width: 100vh ;height:10vh', 'class': 'form-control form-control-lg'}))
